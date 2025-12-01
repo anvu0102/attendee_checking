f591e5b0f82f9ffb05dd71855bc8c8a3cfea46cc
anvu0102 <anvu0102@gmail.com>
2025-12-02 00:39:50 +0700

    Update check.py

--- check.py
+++ check.py
@@ -488,7 +488,7 @@ def main_app(credentials):
     if selected_session:
         
         # TẠO CỘT MỚI: [Tỉ lệ 1, Tỉ lệ 2] => Camera chiếm 1/3 chiều rộng
-        col_camera, col_spacer = st.columns([1, 2])
+        col_camera, col_spacer = st.columns([1, 1])
         
         with col_camera:
             # ĐẶT CAMERA INPUT VÀO CỘT CÓ KÍCH THƯỚC GIỚI HẠN
